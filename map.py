00ba22bbd65422c3e1e4cfd96d8cb7953a6d7abc
Ardent-Mechanic <gikata.38@yandex.ru>
2021-02-23 16:58:19 +0300

    Show map

--- map.py
+++ map.py
@@ -17,8 +17,12 @@ class Tile(pygame.sprite.Sprite):
 
 
 class Map:
-    def __init__(self, screen):
+    def __init__(self, number, screen):
+
         self.screen = screen
+
+        self.level_number = number
+
         self.tile_size = 32
 
         """Test size"""
@@ -26,32 +30,74 @@ class Map:
         self.width = SCREEN_WEIGHT // self.tile_size
         """Test size"""
 
-        self.background = []
-        self.walls_tiles = []
+        self.background_cords = []
+        self.walls_cords = []
+
+        self.background_pic = Image.new('RGB', (SCREEN_WEIGHT, SCREEN_HEIGHT))
+        self.walls_pic = Image.new('RGB', (SCREEN_WEIGHT, SCREEN_HEIGHT))
 
     def render_world(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                pass
-            # gg =pygame.transform.scale(img, (32, 32))
-            # self.screen.blit(img, (x * self.tile_size, y * self.tile_size))
+        self.screen.blit(self.background_pic, (0, 0))
+        self.screen.blit(self.walls_pic, (0, 0))
 
-    def csv_reader(self, num):
-        background, walls_box = [], []
+    def csv_reader(self):
 
-        with open(f"Resources/maps/{num}/new_background.csv") as bg:
+        with open(f"Resources/maps/{self.level_number}/background.csv") as bg:
             data = csv.reader(bg, delimiter=",")
             for row in data:
-                self.background.append(list(row))
+                self.background_cords.append(list(map(int, row)))
 
-        with open(f"Resources/maps/{num}/new_walls.csv") as walls:
+        with open(f"Resources/maps/{self.level_number}/walls.csv") as walls:
             data = csv.reader(walls, delimiter=",")
             for row in data:
-                self.walls_tiles.append(list(row))
+                self.walls_cords.append(list(map(int, row)))
 
-    def load_tiles(self, num):
-        pass
+    def pilImageToSurface(self, pill_image):
+        return pygame.image.fromstring(
+            pill_image.tobytes(), pill_image.size, pill_image.mode).convert()
+
+    def give_level_element(self):
+        return self.pilImageToSurface(self.background_pic), self.pilImageToSurface(self.walls_pic)
+
+    def load_tiles(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.walls_cords[row][col] != -1:
+                    img_num = self.walls_cords[row][col]
+                    self.walls_pic.paste(
+                        Image.open(f"Resources/tileset/{img_num}.png").resize((32, 32), Image.ANTIALIAS),
+                        (col * 32, row * 32))
+
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.background_cords[row][col] != -1:
+                    img_num = self.background_cords[row][col]
+                    self.background_pic.paste(
+                        Image.open(f"Resources/tileset/{img_num}.png").resize((32, 32), Image.ANTIALIAS),
+                        (col * 32, row * 32))
+        self.walls_pic.save("123.png")
+
+    def create_map(self):
+        self.csv_reader()
+        self.load_tiles()
+        self.background_pic, self.walls_pic = self.give_level_element()
 
 
-boom = Map(pygame.display.set_mode((SCREEN_WEIGHT, SCREEN_HEIGHT)))
-boom.render_world()
+if __name__ == '__main__':
+    # boom.render_world()
+    pygame.init()
+    size = width, height = 1024, 1024
+    screen = pygame.display.set_mode(size)
+
+    boom = Map(1, screen)
+    boom.create_map()
+    boom.render_world()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        boom.render_world()
+        pygame.display.flip()
+    pygame.quit()
